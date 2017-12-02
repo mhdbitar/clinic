62dc512410a3cd902ecf218d637ad692a8c1738e
eng.mhd.bitar <mohammad.bitar@das-360.com>
2017-12-02 17:34:03 +0300

    Add CSS file

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hospital</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/app.css">
+</head>
+<body>
+	<div class="topnav" id="myTopnav">
+	  <a href="#home" class="active">Home</a>
+	  <a href="#news">News</a>
+	  <a href="#contact">Contact</a>
+	  <a href="#about">About</a>
+	  <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+	</div>
+
+	<div class="showcase">
+		<img src="assets/img/showcase.jpg" >
+	</div>
+
+	
+</body>
+</html>
